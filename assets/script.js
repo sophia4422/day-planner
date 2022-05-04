a8d4dee675d3eea4216633f4541d9a0d7316a64b
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-05-04 20:16:24 +0100

    moment js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 //accessing date and time
 const currentDateTimeStamp = moment();
 
+//current hour
+const currentHour = moment().hour();
+
 currentDateTimeStamp.format("ddd, Do MMM, YYYY");
+
+console.log(currentDateTimeStamp);
+
+console.log(currentHour);
